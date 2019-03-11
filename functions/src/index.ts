1e022ec7caa7f570a4fe3431664dcea019bf01ae
highjump <lixiang901112@163.com>
2019-03-11 18:14:19 +0800

    Added functions for increasing order count and amount

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,52 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+import * as admin from'firebase-admin';
+admin.initializeApp();
+
+const DASHBOARD_TABLE_NAME = 'dashboard';
+const FIELD_ORDER_AMOUNT = 'order_amount';
+const FIELD_ORDER_COUNT = 'order_count';
+
+
+exports.updateOrderDashboard = functions.database.ref('/orders')
+  .onCreate((snapshot) => {
+    // ref
+    const ref = admin.database().ref(DASHBOARD_TABLE_NAME);
+
+    // increase count
+    const updateCount = ref.child(FIELD_ORDER_COUNT)
+      .transaction((current) => {
+        return (current || 0) + 1;
+      });
+
+    // snapshot
+    //
+    // '-L_gA35kEwOMQhVzAcnK':
+    //    { amount: 1,
+    //      createdAt: 1552298033584,
+    //      userId: 'flvoa9ATJXT06UhfLauXJE9COxn1' }
+    let amount = 0;
+    snapshot.forEach((child) => {
+      console.log(child);
+
+      const info = child.val();
+      amount = info['amount'];
+
+      // stop enumerate
+      return true;
+    });
+
+    const updateAmount = ref.child(FIELD_ORDER_AMOUNT)
+      .transaction((current) => {
+        return (current || 0) + amount;
+      });
+
+    const promises = [updateCount, updateAmount];
+
+    return Promise.all(promises)
+      .then(() => {
+        console.log('Counter updated.');
+      });
+  });
